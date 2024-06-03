89be8746ba7725ee6a69a35fd455e78ae159b080
Amrit-boss <priyanshunarayan74@gmail.com>
2024-06-04 01:26:19 +0530

    added projects

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('maintemplate.html')
+    return render_template('maintemplate .html')
 
 if __name__ == '__main__':
     app.run(debug=True)
